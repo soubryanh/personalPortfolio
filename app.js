9a90b5bba6a873b77f0b2a0d21ed5794e6a88aef
bryan <bryanhernandz102@gmail.com>
2021-09-04 21:24:17 -0500

    changing tab layout

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ document.querySelector(".open").addEventListener("click", () => {
 document.querySelector(".close").addEventListener("click", () => {
   nav.classList.toggle("nav-open");
 });
+
+// Tab Layout
+const changeTab = (e, id) => {
+  let tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("jobcard");
+  for (let index = 0; index < tabcontent.length; index++) {
+    tabcontent[index].classList.remove("active");
+  }
+
+  tablinks = document.getElementsByClassName("tab-title");
+  for (let index = 0; index < tablinks.length; index++) {
+    tablinks[index].classList.remove("active");
+  }
+
+  e.currentTarget.classList.add("active");
+  document.getElementById(id).classList.add("active");
+};
